533cec75c5f34c72b7756ea694ed6631ca991768
asiftm <113905560+asiftm@users.noreply.github.com>
2023-05-06 14:34:31 +0200

    06/05

--- assignments/Birthday/test.py
+++ assignments/Birthday/test.py
@@ -1,6 +1,18 @@
 import numpy as np
 
-file = open('data.csv').read().replace('year,month,date_of_month,day_of_week,births,date\n','').replace('\n,,,,,','').split('\n')
-arr = np.array(file)
+# file = open('data.csv').read().replace('year,month,date_of_month,day_of_week,births,date\n','').replace('\n,,,,,','').split('\n')
+# file.pop(len(file)-1)
+# list_array = []
+# for i in file:
+#     temp=[]
+#     for j in i.split(','):
+#         if '/' not in j:
+#             temp.append(j)
+#     list_array.append(temp)
+# arr = np.array(list_array,dtype='i')
+#
+# print(arr)
 
-print(arr)
+arr = np.loadtxt('data.csv',dtype='U',delimiter=',')
+arr = arr[1:]
+print(arr[:])
