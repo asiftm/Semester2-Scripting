711566e3317352128efcd339e6f19b1985752efe
asiftm <113905560+asiftm@users.noreply.github.com>
2023-05-07 11:32:23 +0200

    h

--- assignments/Birthday/test.py
+++ assignments/Birthday/test.py
@@ -1,6 +1,28 @@
-import numpy as np
+from bs4 import BeautifulSoup
+import requests
+import birthday
+month = 2
+day = 3
 
-file = open('data.csv').read().replace('year,month,date_of_month,day_of_week,births,date\n','').replace('\n,,,,,','').split('\n')
-arr = np.array(file)
+month_name = birthday.get_month_name(month)
+url = f'https://en.wikipedia.org/wiki/{month_name}_{day}'
+response = requests.get(url)
+html_code = response.text
+soup = BeautifulSoup(html_code, 'html.parser')
+tags = soup.find_all('div', {'class': 'mw-parser-output'})
+print(tags)
+temp_lst = (tags[1].getText().split('\n'))
+birth_lst = []
+condition = False;
 
-print(arr)
+file = open(f'{month_name}_{day}.txt', 'w')
+
+for i in temp_lst:
+    if i.strip() == 'Deaths[edit]':
+        break
+    if condition and '[edit]' not in i:
+        file.write(f'{i}\n')
+    if i.strip() == 'Births[edit]':
+        condition = True
+file.close()
+print(f'File saved as {month_name}_{day}.txt')
